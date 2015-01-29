388d96514fb0a13784e1bfcc53922ba7fbfc3c73
Ricardo Nunes de Miranda <nm.ricardo@gmail.com>
2015-01-29 11:18:41 -0200

    Onde comer, parceiros, enderecos e ficha tecnica

---
+++ feverestival2015/MyCalendarTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MyCalendarTableViewCell.m
+//  feverestival2015
+//
+//  Created by Ricardo Nunes de Miranda on 29/01/15.
+//  Copyright (c) 2015 Ricardo Nunes de Miranda. All rights reserved.
+//
+
+#import "MyCalendarTableViewCell.h"
+
+@implementation MyCalendarTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
