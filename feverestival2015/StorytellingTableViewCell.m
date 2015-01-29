39a8f27ec381ff267a9dd4f781a1ae8baf332c32
Ricardo Nunes de Miranda <nm.ricardo@gmail.com>
2015-01-29 15:53:54 -0200

    Versao final

---
+++ feverestival2015/StorytellingTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  StorytellingTableViewCell.m
+//  feverestival2015
+//
+//  Created by Ricardo Nunes de Miranda on 29/01/15.
+//  Copyright (c) 2015 Ricardo Nunes de Miranda. All rights reserved.
+//
+
+#import "StorytellingTableViewCell.h"
+
+@implementation StorytellingTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ feverestival2015/StorytellingViewController.m
@@ -0,0 +1,37 @@
+//
+//  StorytellingViewController.m
+//  feverestival2015
+//
+//  Created by Ricardo Nunes de Miranda on 29/01/15.
+//  Copyright (c) 2015 Ricardo Nunes de Miranda. All rights reserved.
+//
+
+#import "StorytellingViewController.h"
+
+@interface StorytellingViewController ()
+
+@end
+
+@implementation StorytellingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
